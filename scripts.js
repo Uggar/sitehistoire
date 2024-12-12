52cdfa8f2efc23aa078970957c13dc4815be74d7
Uggar <165919524+Uggar@users.noreply.github.com>
2024-12-12 11:39:46 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuLinks = document.querySelectorAll('.menu-link');
-    const contentSections = document.querySelectorAll('.content-section');
-
-    menuLinks.forEach(function (link) {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
-
-            contentSections.forEach(function (section) {
-                if (section !== targetSection) {
-                    section.classList.remove('show');
-                }
-            });
-
-            targetSection.classList.toggle('show');
-        });
-    });
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Merci pour votre message, nous reviendrons vers vous bientôt !');
+    document.getElementById('contactForm').reset();
 });
